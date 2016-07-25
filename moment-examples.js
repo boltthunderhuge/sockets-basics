99c2e4a56271942609d52afde4f264397764321b
Graeme Anderson <graemeanderson@kabaminc.com>
2016-07-25 09:01:48 -0700

    add moment example

---
+++ moment-examples.js
@@ -0,0 +1,23 @@
+var moment = require('moment');
+
+var now = moment();
+
+console.log(now.format());
+/*//now.subtract(1, 'year');
+console.log(now.format('YYYY'));
+console.log(now.format('h:mma'));
+console.log(now.format('MMM Do YYYY, h:mma'));
+console.log(now.format('X'));*/
+console.log(now.format('x'));
+console.log(now.valueOf());
+
+var timestamp = 1469461567012;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.format());
+
+function timeStamp2Time(timestamp) {
+	return moment.utc(timestamp).local().format('h:mma');
+}
+
+console.log("Timestamp is " + timeStamp2Time(timestamp));
